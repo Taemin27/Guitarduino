4107e444c6de8a12f2815e665f9a3e15e4052f8e
Taemin27 <taeminshin0708@gmail.com>
2023-12-23 13:36:09 +0900

    Libraries and images

--- Guitarduino/Guitarduino.ino
+++ Guitarduino/Guitarduino.ino
@@ -1,63 +1,31 @@
-#include <Audio.h>
-#include <Wire.h>
-#include <SPI.h>
-#include <SD.h>
-#include <SerialFlash.h>
-
-// GUItool: begin automatically generated code
-AudioInputI2S            i2s1;           //xy=643,397
-AudioAmplifier           amp1;           //xy=845,386
-AudioEffectWaveshaper    waveshape1;     //xy=1044,474
-AudioMixer4              mixer1;         //xy=1213,382
-AudioOutputI2S           i2s2;           //xy=1420,379
-AudioConnection          patchCord1(i2s1, 1, amp1, 0);
-AudioConnection          patchCord2(amp1, waveshape1);
-AudioConnection          patchCord3(amp1, 0, mixer1, 0);
-AudioConnection          patchCord4(waveshape1, 0, mixer1, 1);
-AudioConnection          patchCord5(mixer1, 0, i2s2, 1);
-AudioControlSGTL5000     sgtl5000_1;     //xy=1036,249
-// GUItool: end automatically generated code
+#include <Adafruit_ST7735.h>
+#include <Adafruit_GFX.h>
+#include <Encoder.h>
 
 
+String pages[] = { "Tuner", "effects", "metronome", "backingTrack", "record" };
 
-#define COUNT_OF(x) ((int32_t)(sizeof x / sizeof x[0]))
+#define TFT_CS  4
+#define TFT_RST 9
+#define TFT_DC 5
 
-bool bypass = true;
 
-float shape[] =  {-1.0, -0.9, 0.0, 0.7, 0.9, 1.0, 1.2, 1.0, 0.8};
+// Encoder
+#define outputA 2
+#define outputB 3
+int aState;
+int aLastState;
+
 
 void setup() {
+  pinMode(outputA, INPUT);
+  pinMode(outputB, INPUT);
+
   Serial.begin(9600);
-  pinMode(1, INPUT_PULLUP);
-
-  AudioMemory(100);
-  
-  sgtl5000_1.enable();
-  sgtl5000_1.volume(1.0);
-
-  mixer1.gain(0, 1);
-  mixer1.gain(1, 0);
-
-  amp1.gain(1.5);
-
-  //waveform1.begin(1, 220, WAVEFORM_SINE);
-
-  waveshape1.shape(shape,COUNT_OF(shape));
+  aLastState = digitalRead(outputA);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if(digitalRead(1) == LOW) {
-    bypass = !bypass;
-    if(bypass) {
-      mixer1.gain(0, 1);
-      mixer1.gain(1, 0);
-      Serial.println("Bypass on");
-    } else {
-      mixer1.gain(0, 0);
-      mixer1.gain(1, 1);
-      Serial.println("Bypass off");
-    }
-    delay(300);
-  }
+
 }
+
